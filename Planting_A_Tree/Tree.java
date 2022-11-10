827b7d453ea853a5233b341ade810fedc6e44320
Kas Eam <KassandraEam@gmail.com>
2022-11-10 15:18:16 -0600

    Planting a tree project on Codecademy. Tasked to create a class, then a main method, then print a few lines that represent a tree.

---
+++ Planting_A_Tree/Tree.java
@@ -0,0 +1,12 @@
+// Define your tree class in this file!
+public class Tree {
+    public static void main (String[] args) {
+        System.out.println("My name is Kas!");
+        System.out.println("Java is interesting!");
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+    }
+}
