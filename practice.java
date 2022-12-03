fe446418bed77b6cb7527f8b2de4e82745bc61ee
Kas Eam <KassandraEam@gmail.com>
2022-12-03 15:52:44 -0600

    WIP

--- practice.java
+++ practice.java
@@ -2,5 +2,8 @@ public class practice {
     public static void main(String[] args) {
         int num = (10 - (4 + 3)) * 6;
         System.out.println(num);
+        int numTwo = (10 + 5);
+        System.out.println(numTwo);
     }
 }
+
