392100826d00eacc5445621d2961ee81211c31ea
Kas Eam <KassandraEam@gmail.com>
2023-01-06 16:15:19 -0600

    Added new class

---
+++ Zoo.java
@@ -0,0 +1,8 @@
+public class Zoo {
+    public static void main(String[] args) {
+        int animals = 12;
+        String species = "zebra";
+        System.out.println(species);
+        System.out.println(animals);
+    }
+}
