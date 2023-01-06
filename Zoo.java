5c2cb209d082f1a1694cec62cc92ac243ad2b6e1
Kas Eam <KassandraEam@gmail.com>
2023-01-06 16:23:16 -0600

    Concatenating string and primitive types.

--- Zoo.java
+++ Zoo.java
@@ -2,7 +2,7 @@ public class Zoo {
     public static void main(String[] args) {
         int animals = 12;
         String species = "zebra";
-        System.out.println(species);
-        System.out.println(animals);
+        String zooDescription = "Our zoo has " + animals + " " + species + "s!";
+        System.out.println(zooDescription);
     }
 }
