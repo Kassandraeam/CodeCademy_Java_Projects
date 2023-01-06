b5b13c5504660ed356d065439be70cd55e7d246a
Kas Eam <KassandraEam@gmail.com>
2023-01-06 15:47:51 -0600

    .equals() method

--- practice.java
+++ practice.java
@@ -1,9 +1,14 @@
 public class practice {
     public static void main(String[] args) {
-        int num = (10 - (4 + 3)) * 6;
-        System.out.println(num);
-        int numTwo = (10 + 5);
-        System.out.println(numTwo);
+        // int num = (10 - (4 + 3)) * 6;
+        // System.out.println(num);
+        // int numTwo = (10 + 5);
+        // System.out.println(numTwo);
+        String line1 = "Nah nah nah nah nah nah nah nah nah yeah";
+        String line2 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
+        String line3 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
+        System.out.println(line1.equals(line2));
+        System.out.println(line2.equals(line3));
+
     }
 }
-
